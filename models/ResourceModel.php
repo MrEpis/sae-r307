a9b40fdc72357d4d696cc05581e04fcc9a0b56e9
Coletta <leo.coletta@etu.unice.fr>
2025-12-28 19:37:53 +0100

    db -> DataBase

--- models/ResourceModel.php
+++ models/ResourceModel.php
@@ -1,11 +1,11 @@
 <?php
-require_once __DIR__ . '/../config/db.php';
+require_once __DIR__ . '/../config/Database.php';
 
 class ResourceModel {
     private $db;
 
     public function __construct() {
-        // Utilise l'instance PDO définie dans config/db.php
+        // Utilise l'instance PDO définie dans config/DataBase.php
         $this->db = Database::getInstance();
     }
 
